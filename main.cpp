c9021071a6883165984d1e8f5c6f718f39a4c19c
ben-humphries <ben_humphries@caryacademy.org>
2017-12-31 01:24:52 -0500

    Added vertex shader stuff but it doesn't work yet

--- main.cpp
+++ main.cpp
@@ -19,10 +19,42 @@ int main(){
   glewExperimental = GL_TRUE;
   glewInit();
 
-  GLuint vertexBuffer;
-  glGenBuffers(1, &vertexBuffer);
 
-  printf("%u\n", vertexBuffer);
+  float vertices[] = {
+    0.0f, 0.5f,
+    0.5f, -0.5f,
+    -0.5f, -0.5f
+
+  };
+
+  
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  const char* vertexSource = R"glsl(
+  #version 150 core
+
+  in vec2 position;
+
+  void main()
+  {
+  gl_Position = vec4(position, 0.0, 1.0);
+  }
+  )glsl";
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexSource, NULL);
+
+  glCompileShader(vertexShader);
+
+  GLint status;
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+
+  if(!status == GL_TRUE){
+    printf("Vertex shader was not compiled successfully!\n");
+  }
   
   bool running = true;
   while(running){
