a2d7c916d7013f3e5e00f4fb9197cff0c60fe9d5
ben-humphries <ben_humphries@caryacademy.org>
2017-12-31 10:27:03 -0500

    Added fragment and vertex shaders

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Window.hpp>
 #include <GL/glew.h>
 
+#include <fstream>
 
 int main(){
 
@@ -33,27 +34,57 @@ int main(){
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  const char* vertexSource = R"glsl(
-  #version 150 core
-
-  in vec2 position;
-
-  void main()
-  {
-  gl_Position = vec4(position, 0.0, 1.0);
+  //Open and compile vertex shader
+  FILE * vertexFile = fopen("shaders/vertex.glsl", "r");
+  if (vertexFile == NULL) {
+    fprintf(stderr, "Could not load vertex shader.\n");
+    return -1;
   }
-  )glsl";
+  int len = 0;
+  while (fgetc(vertexFile) != EOF)
+    len++;
+  fseek(vertexFile, 0, SEEK_SET);
+  char * vertex = (char*) malloc(sizeof(char) * len + 1);
+  fread(vertex, len, sizeof(char), vertexFile);
+  vertex[len] = '\0';
+
 
   GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexSource, NULL);
+  glShaderSource(vertexShader, 1, &vertex, NULL);
 
   glCompileShader(vertexShader);
+
+  //Open and compile fragment shader
+  FILE * fragmentFile = fopen("shaders/fragment.glsl", "r");
+  if (fragmentFile == NULL) {
+    fprintf(stderr, "Could not load fragment shader.\n");
+    return -1;
+  }
+  len = 0;
+  while (fgetc(fragmentFile) != EOF)
+    len++;
+  fseek(fragmentFile, 0, SEEK_SET);
+  char * fragment = (char*) malloc(sizeof(char) * len + 1);
+  fread(fragment, len, sizeof(char), fragmentFile);
+  fragment[len] = '\0';
+
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragment, NULL);
+
+  glCompileShader(fragmentShader);  
 
   GLint status;
   glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
 
-  if(!status == GL_TRUE){
-    printf("Vertex shader was not compiled successfully!\n");
+  if(status == GL_TRUE){
+    printf("Vertex shader was compiled successfully!\n");
+  }
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+
+  if(status == GL_TRUE){
+    printf("Fragment shader was compiled successfully!\n");
   }
   
   bool running = true;
